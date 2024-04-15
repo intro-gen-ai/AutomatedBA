9bbbf40ec6a0dfc5965506303525798e2d0c504e
ljgries <Luke.j.gries@vanderbilt.edu>
2024-04-15 16:56:10 -0500

    Revert "Included definitions in schemas"
    This reverts commit 45ced7ec92cab729655adfc6decf5d6f7c6173cf.

--- src/prompt/prompt_text/prompt_anthropic.md
+++ src/prompt/prompt_text/prompt_anthropic.md
@@ -1,23 +1,11 @@
 ### Instructions:
-
 Your task is to convert a text question to a SQL query that runs on Snowflake, given a database schema. Return the SQL as a markdown string, nothing else.
 
-### Operational Definitions
-
-The following definitions are important to understand in context:
-{rag_context}
-
 ### Input:
-
 `{preprompt}`: `{user_question}`.
 `{instructions}`
 This query will run on a database whose schema is represented in this string:
 {table_metadata_string}
-
 ### Response:
-
-Given the database schema and context, here is the SQL query that answers `{user_question}`:
-
+Given the database schema, here is the SQL query that answers `{user_question}`:
 ```sql
-
-```
--- test/test.py
+++ test/test.py
@@ -4,39 +4,30 @@ import os
 
 path = os.path.dirname(os.path.realpath(__file__))
 
-df = pd.read_csv(os.path.join(path, "questions_gen_snowflake.csv"))
+df = pd.read_csv(os.path.join(path, 'questions_gen_snowflake.csv'))
 results = []
 for index, row in df.iterrows():
-    print("H")
-    database = row["db_name"]
-    question = row["question"]
-
-    gt_query = row["query"]
-
-    # TODO: bunch of stuff
+    database = row['db_name']
+    question = row['question']
+    
+    gt_query = row['query']
+    
+    #TODO: bunch of stuff
     """ 1. run model on user question and database schema. we can set the parameters we're testing
         2. get snowflake output from both gt_query and model output
         3. get gt and model snowflake results as a dataframe. Search : cursor.fetch_pandas_all()
         
         not sure how snowflake handles errors, may need try-catch. return empty dataframe if query results in an error
     """
-
-    gt_result = pd.DataFrame(df)  # DUMMY RESULT, CHANGE
-    model_result = pd.DataFrame(df)  # DUMMY RESULT, CHANGE
-
+    
+    gt_result = pd.DataFrame(df) # DUMMY RESULT, CHANGE
+    model_result = pd.DataFrame(df) # DUMMY RESULT, CHANGE
+    
     # Equality check
-    gt_result_sorted = gt_result.sort_values(by=gt_result.columns.tolist()).reset_index(
-        drop=True
-    )
-    model_result_sorted = model_result.sort_values(
-        by=model_result.columns.tolist()
-    ).reset_index(drop=True)
-    results.append(
-        (gt_result_sorted.equals(model_result_sorted), database, row["query_category"])
-    )
-
-    result_df = pd.DataFrame(
-        results, columns=["eqauality", "database", "query_category"]
-    )
-
-    result_df.to_csv(os.path.join(path, "results.csv"), encoding="utf-8", index=False)
+    gt_result_sorted = gt_result.sort_values(by=gt_result.columns.tolist()).reset_index(drop=True)
+    model_result_sorted = model_result.sort_values(by=model_result.columns.tolist()).reset_index(drop=True)
+    results.append((gt_result_sorted.equals(model_result_sorted),database, row['query_category']))
+    
+    result_df = pd.DataFrame(results, columns =['eqauality', 'database', 'query_category'])
+    
+    result_df.to_csv(os.path.join(path, 'results.csv'), encoding='utf-8', index=False)
